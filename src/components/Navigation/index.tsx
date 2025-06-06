1afdb6bdb1119af9844661be50959522b224976c
Yevhenii <ke1vin.kapustian@gmail.com>
2025-06-07 00:07:42 +0200

    Fixed responsive

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -4,6 +4,7 @@ import './navigation.scss'
 import NavigationItem from "./NavigationItem";
 import { getPages, getRoutes } from "@/utils/functions";
 import { Icon } from "@iconify-icon/react/dist/iconify.mjs";
+import Image from "next/image";
 
 const Navigation = (): ReactElement => {
     const [language, setLanguage] = useState<string>('en')
@@ -25,18 +26,27 @@ const Navigation = (): ReactElement => {
 
             </nav>
             <nav className="mobileNav" onClick={handleMobileMenu}>
-                <Icon icon="material-symbols:menu-rounded" width="42" height="42" />
+                <Icon className="mobileNav__burgerIcon" icon="material-symbols:menu-rounded" width="42" height="42" />
 
                 {mobileOpen &&
                     <div className="mobileNav__background">
                         <div className="mobileNav__background-container">
-                            <img className="mobileNav__background-logo" src="logoMobile.png" alt="nope" />
-                            <div className="mobileNav__background-links">
-                                {getPages('mobile').map((item: string, index: number) => <NavigationItem name={item} route={getRoutes('mobile')[index]} key={index} />)}
+                            <div className="mobileNav__background-decoreUpper">
+                                <img src="golddecore.png" alt="" width={121} height={134} />
+                            </div>
+                            <div className="mobileNav__background-container__items">
+                                <div className="mobileNav__background-logo">
+                                    <img src="logoMobile.png" alt="nope" />
+                                </div>
+
+                                <div className="mobileNav__background-links">
+                                    {getPages('mobile').map((item: string, index: number) => <NavigationItem name={item} route={getRoutes('mobile')[index]} key={index} />)}
+                                </div>
+                            </div>
+                            <div className="mobileNav__background-decoreLower">
+                                <img src="golddecore.png" alt="" width={121} height={134} />
                             </div>
                         </div>
-                        <img className="mobileNav__background-decoreUpper" src="golddecore.png" alt="" width={121} height={134} />
-                        <img className="mobileNav__background-decoreLower" src="golddecore.png" alt="" width={121} height={134} />
                     </div>
                 }
             </nav>
