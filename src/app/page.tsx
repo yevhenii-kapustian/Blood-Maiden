ed8f75cf34a8ba2afde3b2a0fa0cb8846b7cd7cc
PriyeshIllam <seeujob@gmail.com>
2025-06-08 23:47:19 +0200

    cleaned comments and unnecessary codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-// app/page.tsx (or wherever your main page is)
-
 import LowerHome from "@/components/LowerHomeSection";
 
 export default function HomePage() {
   return (
     <main>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Crimson+Pro:ital,wght@0,200..900;1,200..900&display=swap"
-          rel="stylesheet"
-        />
-      {/* Any other content */}
       <LowerHome />
     </main>
   );
