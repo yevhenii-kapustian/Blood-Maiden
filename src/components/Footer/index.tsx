0d38c7dbf6726ea596207af50c5ef415db4f981c
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-06-10 18:58:32 +0200

    adding images on footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -16,16 +16,16 @@ const Footer = () => {
       </div>
       
       <div className="right">
-        <Image src="/assets/Pegi18.png" alt="Pegi18.png" width={100} height={100} />
-        <Image src="/assets/Fear.png" alt="Fear" width={100} height={100} />
-        <Image src="/assets/BadLanguage.png" alt="Bad Language" width={100} height={100} />
-        <Image src="/assets/Violence.png" alt="Violence" width={100} height={100} />
-        <Image src="/assets/P.png" alt="Available on PC" width={100} height={100} />
+        <Image src="Pegi18.png" alt="Pegi18.png" width={100} height={100} />
+        <Image src="Fear.png" alt="Fear" width={100} height={100} />
+        <Image src="BadLanguage.png" alt="Bad Language" width={100} height={100} />
+        <Image src="Violence.png" alt="Violence" width={100} height={100} />
+        <Image src="P.png" alt="Available on PC" width={100} height={100} />
       </div>
       </div>
 
       <div className="bottom">
-      <Image src="/assets/Ichio-Footer.png" alt="itch.io" width={24} height={24} />
+      <Image src="Ichio-Footer.png" alt="itch.io" width={24} height={24} />
       <a href="https://itch.io/">itch.io</a>  
       <p className="center">
         © 2025 Group 7 Studios. All Rights Reserved.
