04f91821a64eedc6ce7a4736314e8592711f9cd8
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-06-13 14:18:10 +0200

    added folders and copied previous code

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
 import AboutIntro from "@/components/AboutIntro";
+import AboutMainSection from "@/components/AboutMainSection";
 import './about.scss'
 
 const About = () => {
     return (
         <div className="about">
             <AboutIntro />
+            <AboutMainSection />
         </div>
     )
 }
