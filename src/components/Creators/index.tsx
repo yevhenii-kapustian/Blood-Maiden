2ab87e41b54f95a36d1b1ada16eb66e2ce9e929f
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-06-13 11:13:35 +0200

    feature/014-content cretors adding stuffs

--- src/components/Creators/index.tsx
+++ src/components/Creators/index.tsx
@@ -3,25 +3,25 @@ import React from 'react';
 import './creators.scss';
 import { gameCreators } from '@/data/data';
 
-
-
 const Creators = () => {
   return (
-    <div className="creators-container">      
-    <h2 className="section-title">CONTENT CREATORS</h2>
-
+    <div className="creators-container">    
+     <h2 className="section-title">CONTENT CREATORS</h2>  
+  
       <div className="creators-grid">
+        <div className="grid-card">
         <ul>
         <h3 >MANAGEMENT</h3>
         {gameCreators.get("management")?.map((item, index) => (
         <li key={index}>
         <ul>
-        <li >{item.name} - {item.role}</li>
+        <li>{item.name} - {item.role}</li>
         <li className="list-menu"><a href={item.link}>{item.name} | LinkedIn </a></li>
         </ul>
         </li>
        ))}
        </ul>
+       </div>
 
 
       
