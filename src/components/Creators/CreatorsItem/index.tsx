ee697239226d39a91e1815b8e803b72652f2969a
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-06-16 11:54:35 +0200

    added some lineup and helped ankita with problems

--- src/components/Creators/CreatorsItem/index.tsx
+++ src/components/Creators/CreatorsItem/index.tsx
@@ -11,17 +11,23 @@ const CreatorsItem = ({ creatorsTeam, title }: ICreatorsItem) => {
             <div className="grid-card">
                 <ul className="grid-item">
                     <h3>{title}</h3>
+                    <div className="grid-border">
                     {creatorsTeam?.map((item, index) => (
-                        <li key={index}>
+                        <div key={index}>
                             <ul>
                                 <li>{item.name} - {item.role}</li>
-                                <li className="list-menu"><a href={item.link}>{item.name} | LinkedIn </a></li>
-                            </ul>
-                        </li>
+                                <div className="centerItems">
+                                    <img className="linkedInIcon" src="Vector.png" alt="linkedin icon" width="25px" height="25px" />
+                                    <a className="linkedInItem" href={item.link}>{item.name} | LinkedIn </a>
+                            </div>
+                            {/* <li className="list-menu"><a href={item.link}>{item.name} | LinkedIn </a></li> */}
+                        </ul>
+                        </div>
                     ))}
-                </ul>
-            </div>
+                    </div>
+            </ul>
         </div>
+        </div >
     )
 }
 export default CreatorsItem;
