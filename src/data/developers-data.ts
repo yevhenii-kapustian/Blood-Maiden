c450e94bffe06ae55baf3f285bc6726b454215e4
todatoday <taki.souad-90@hotmail.com>
2025-06-16 09:48:36 +0200

    Creating Data Files for Developers

---
+++ src/data/developers-data.ts
@@ -0,0 +1,29 @@
+import { Developer } from "@/types/type";
+
+const people: Developer[] = [
+  {
+    name: "",
+    link: "",
+    image: ""
+  },
+  {
+    name: "",
+    link: "",
+    image: ""
+  },
+  {
+    name: "",
+    link: "",
+    image: ""
+  },
+  {
+    name: "Yevhenii Kapustian",
+    link: "https://www.linkedin.com/in/yevhenii-kapustian-ab85a8279?trk=contact-info",
+    image: ""
+  },
+  {
+    name: "",
+    link: "",
+    image: ""
+  }
+];
