48fee51b6a5389f08a54d37e6b09215b3e47764d
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-06-23 22:34:53 +0200

    fixing

--- src/components/EnemiesAndObstacles/index.tsx
+++ src/components/EnemiesAndObstacles/index.tsx
@@ -7,7 +7,7 @@ import { monsters } from '@/data/data';
 const EnemiesAndObstacles: React.FC = () => {
   return (
     <div id='enemies' className="level-monster">
-      <h1 className="monster-title">Enemies and obstacles</h1>
+      <h2 className="monster-title">Enemies and obstacles</h2>
 
       <div className="monster-section">
         <div className="monster-grid">
